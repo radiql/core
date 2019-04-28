d9b9f4f3185cd92a1b4f95c14d00e81620a41d89
RADiQL Dev <42944942+radiql@users.noreply.github.com>
2019-04-28 12:52:18 +0100

    Create interactors.go

---
+++ src/core/interactors.go
@@ -0,0 +1,8 @@
+package core
+
+// Interactor is the interface to be implemented by Interactor components
+// from Robert C. Martin's Clean Architecture.
+type Interactor interface {
+  Process()
+}
+
