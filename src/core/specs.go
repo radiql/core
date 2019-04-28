2dc5c857d452983f77c0f9f65a2f927f583578e2
RADiQL Dev <42944942+radiql@users.noreply.github.com>
2019-04-28 21:45:47 +0100

    Update specs.go

--- src/core/specs.go
+++ src/core/specs.go
@@ -1,59 +1,6 @@
 
 package core
 
-type CreateMsg struct {
-  Interactor CompositeInteractorSpec
-  
-  Version VersionSpec
-  Build   BuildSpec
-  Package PackageSpec
-  Deploy  DeploymentSpec
-  Meta    Metadata
-}
-
-type UpdateMsg struct {
-  Interactor CompositeInteractorSpec
-  
-  Version VersionSpec
-  Build   BuildSpec
-  Package PackageSpec
-  Deploy  DeploymentSpec
-  Meta    Metadata
-}
-
-type DeprecateMsg struct {
-  TypeRef InteractorTypeRef
-  Meta    Metadata
-}
-
-type ArchiveMsg struct {
-  TypeRef InteractorTypeRef
-  Meta    Metadata
-}
-
-type BuildMsg struct {
-  TypeRef InteractorTypeRef
-  Meta    Metadata
-}
-
-type DeployMsg struct {
-  TypeRef InteractorTypeRef
-  Meta    Metadata
-}
-
-type RedeployMsg struct {
-  TypeRef InteractorTypeRef
-  Meta    Metadata
-}
-
-type UndeployMsg struct {
-  TypeRef InteractorTypeRef
-  Meta    Metadata
-}  
-
-
-type Metadata map[string]string
-
 
 type InteractorTypeRef struct {
   Location  string
